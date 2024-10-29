794b92daddf3410ae9a8ee0a8405729c1c9c769f
Debdal Chowdhury <53616883+debdal6@users.noreply.github.com>
2024-10-29 04:46:02 +0000

    Variable names updated and spacing fixed 1D FTCS script

--- heat-eq-simulation/1D_ftcs.py
+++ heat-eq-simulation/1D_ftcs.py
@@ -10,43 +10,41 @@ import matplotlib.pyplot as pyPlot
 lengthOfRod = 10
 maxTime = 1
 heatConductivity = 1 #100 is going the least in nagtive and 1000 produces the same graph to my eye
-numPointsSpace = 200
-numPointsTime = 2000
+numPointimeSpaceSpace = 200
+numPointimeSpaceTime = 2000
 
 # Length Vector plotted on x-axis
-xs = numpy.linspace(0, lengthOfRod, numPointsSpace)
+xSpace = numpy.linspace(0, lengthOfRod, numPointimeSpaceSpace)
 # Time Vector plotted on y-axis
-ts = numpy.linspace(0, maxTime, numPointsTime)
+timeSpace = numpy.linspace(0, maxTime, numPointimeSpaceTime)
 
-timeStepSize = ts[1] - ts[0]
-spaceStepSize = xs[1] - xs[0]
+timeStepSize = timeSpace[1] - timeSpace[0]
+spaceStepSize = xSpace[1] - xSpace[0]
 
 boundaryConditions = [numpy.sin(lengthOfRod)+numpy.sin(maxTime), 
-                    numpy.sin(lengthOfRod)]
-intialConditions = numpy.sin(xs)
+                    	numpy.sin(lengthOfRod)]
+intialConditions = numpy.sin(xSpace)
 
-xsLength = len(xs)
-tsLength = len(ts)
+xSpaceLength = len(xSpace)
+timeSpaceLength = len(timeSpace)
 
 
 # Empty Matrix/NestedList with zeroes
-gridMatrix = numpy.zeros((xsLength, tsLength))
-gridMatrix[0,:] = boundaryConditions[0]
-gridMatrix[-1,:] = boundaryConditions[1]
-gridMatrix[:, 0] = intialConditions
+tempMatrix = numpy.zeros((xSpaceLength, timeSpaceLength))
+tempMatrix[0,:] = boundaryConditions[0]
+tempMatrix[-1,:] = boundaryConditions[1]
+tempMatrix[:, 0] = intialConditions
 
-factor=(heatConductivity*timeStepSize)/spaceStepSize**2
-print(factor)
+diffusivityConstant = (heatConductivity * timeStepSize) / spaceStepSize**2
+print(diffusivityConstant)
 
-for tau in range (1, tsLength-1):
-    for j in range (1, xsLength-1):
-        
-        gridMatrix[j,tau]=(factor*
-                (gridMatrix[j-1,tau-1]-
-                 2*gridMatrix[j,tau-1]+
-                 gridMatrix[j+1,tau-1])
-            ) + gridMatrix[j,tau-1]
+for tau in range (1, timeSpaceLength-1):
+	for j in range (1, xSpaceLength-1):
+		tempMatrix[j,tau] = (
+				diffusivityConstant * (
+					tempMatrix[j-1,tau-1] - 2 * tempMatrix[j,tau-1] + tempMatrix[j+1,tau-1]
+				)
+			) + tempMatrix[j,tau-1]
 
-
-pyPlot.plot(gridMatrix)
+pyPlot.plot(tempMatrix)
 pyPlot.show()
