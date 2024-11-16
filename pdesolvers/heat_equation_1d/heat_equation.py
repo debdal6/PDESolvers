8e790defa06ef9c01a231b92c3e3f9e064781a67
Chelsea De Marseilla <chemardes@Chelseas-Air.lan>
2024-11-16 11:58:59 +0000

    FEATURE: fixing error handling + condition checks

--- pdesolvers/heat_equation_1d/heat_equation.py
+++ pdesolvers/heat_equation_1d/heat_equation.py
@@ -18,24 +18,12 @@ class HeatEquation:
         self.__right_boundary_temp = None
         self.__u = None
 
-    def __check_conditions(self):
-        if self.__initial_temp is None:
-            return ValueError("Initial Temperature has not been initialised")
-
-        if self.__left_boundary_temp is not None:
-            err = np.abs(self.__left_boundary_temp(0) - self.__initial_temp(0))
-            assert err < 1e-12
-
-        if self.__right_boundary_temp is not None:
-            err = np.abs(self.__right_boundary_temp(0) - self.__initial_temp(0))
-            assert err < 1e-12
-
     def set_solver(self, strategy: SolverStrategy):
         self.__solver = strategy
 
     def solve(self):
         if self.__solver is None:
-            return RuntimeError("Solver has not been set. Please pick one")
+            raise RuntimeError("Solver has not been set. Please pick one")
 
         self.__solver.solve(self)
         return self
@@ -43,7 +31,7 @@ class HeatEquation:
     def plot(self):
 
         if self.__u is None:
-            return RuntimeError("Solution has not been computer - Please run the solver.")
+            raise RuntimeError("Solution has not been computed - please run the solver.")
 
         x = self._generate_x_grid()
         t = self._generate_t_grid()
@@ -63,22 +51,41 @@ class HeatEquation:
         plt.show()
 
     def set_initial_temp(self, u0):
+        self.__validate_callable(u0)
         if self.__length is None:
-            return RuntimeError("Rod length has not been initialised.")
+            raise RuntimeError("Rod length has not been initialised.")
         self.__initial_temp = u0
         self.__check_conditions()
         return self
 
     def set_left_boundary_temp(self, left):
+        self.__validate_callable(left)
         self.__left_boundary_temp = left
         self.__check_conditions()
         return self
 
     def set_right_boundary_temp(self, right):
+        self.__validate_callable(right)
         self.__right_boundary_temp = right
         self.__check_conditions()
         return self
 
+    def __check_conditions(self):
+        if self.__initial_temp is None:
+            raise ValueError("Initial Temperature has not been initialised")
+
+        if self.__left_boundary_temp is not None:
+            err = np.abs(self.__left_boundary_temp(0) - self.__initial_temp(0))
+            assert err < 1e-12
+
+        if self.__right_boundary_temp is not None:
+            err = np.abs(self.__right_boundary_temp(0) - self.__initial_temp(0))
+            assert err < 1e-12
+
+    @staticmethod
+    def __validate_callable(func):
+        if not callable(func):
+            raise ValueError("Temperature conditions must be a callable function")
 
     def _generate_x_grid(self):
         return np.linspace(0, self.__length, self.__x_nodes)
