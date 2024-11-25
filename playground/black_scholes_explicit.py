9a9ac352ba6c52ba376214d9f9bcfd4cab7a03c5
chemardes <cdmrs08@gmail.com>
2024-11-25 16:06:03 +0000

    FEATURE: modifying to take in compute for put options + comments

--- playground/black_scholes_explicit.py
+++ playground/black_scholes_explicit.py
@@ -4,9 +4,28 @@ from matplotlib import pyplot as plt
 
 class BlackScholesExplicitSolver():
 
-    # using call option as an example for now - will update it to toggle between put/call
+    """
+    The Black Scholes equation is a Partial Differential Equation (PDE) used to determine the fair value price of an
+    option over time.
 
-    def __init__(self, S_max, expiry, sigma, r, K, s_nodes, t_nodes = None):
+    The solver takes into account two different option types - call and put options
+    """
+
+    def __init__(self, option_type, S_max, expiry, sigma, r, K, s_nodes, t_nodes = None):
+        """
+        Initialises the solver with the necessary parameters
+
+        :param option_type: the type of option
+        :param S_max: maximum asset price in the grid
+        :param expiry: time to maturity/expiry of the option
+        :param sigma: volatility of the asset
+        :param r: risk-free interest rate
+        :param K: strike price
+        :param s_nodes: number of asset price nodes
+        :param t_nodes: number of time nodes
+        """
+
+        self.__option_type = option_type
         self.__S_max = S_max
         self.__expiry = expiry
         self.__sigma = sigma
@@ -17,6 +36,12 @@ class BlackScholesExplicitSolver():
         self.__V = None
 
     def solve(self):
+        """
+        This method solves the Black-Scholes equation using the explicit finite difference method
+        :return: the solver instance with the computed option values
+        """
+
+        q = self.__check_option_type()
 
         if self.__t_nodes is None:
             dt_max = 1/((self.__s_nodes**2) * (self.__sigma**2)) # cfl condition to ensure stability
@@ -25,7 +50,7 @@ class BlackScholesExplicitSolver():
             dt = self.__expiry / self.__t_nodes # to ensure that the expiration time is integer time steps away
         else:
             # possible fix - set a check to see that user-defined value is within cfl condition
-            dt = self.__expiry/self.__t_nodes
+            dt = self.__expiry/ self.__t_nodes
 
         S = self.__generate_asset_grid()
         T = self.__generate_time_grid()
@@ -34,7 +59,8 @@ class BlackScholesExplicitSolver():
 
         V = np.zeros((self.__s_nodes+1, self.__t_nodes+1))
 
-        V[:,-1] = np.maximum(S - self.__K, 0)
+        # setting terminal condition
+        V[:,-1] = np.maximum(q * (S - self.__K), 0)
 
         for tau in reversed(range(self.__t_nodes)):
             for i in range(1, self.__s_nodes):
@@ -43,8 +69,10 @@ class BlackScholesExplicitSolver():
                 theta = -0.5 * ( self.__sigma ** 2) * (S[i] ** 2) * gamma - self.__r * S[i] * delta + self.__r * V[i, tau+1]
                 V[i, tau] = V[i, tau + 1] - (theta * dt)
 
-            V[0, tau] = 0
-            V[self.__s_nodes, tau] = self.__S_max - self.__K * np.exp(-self.__r * (self.__expiry - T[tau]))
+            # setting boundary conditions
+            lower, upper = self.__set_boundary_conditions(q, T, tau)
+            V[0, tau] = lower
+            V[self.__s_nodes, tau] = upper
 
         self.__V = V
 
@@ -71,17 +99,38 @@ class BlackScholesExplicitSolver():
         plt.show()
 
 
+    def __check_option_type(self):
+
+        if self.__option_type == 'call':
+            q = 1
+        elif self.__option_type == 'put':
+            q = -1
+        else:
+            raise ValueError("Invalid option type - Option type should either be 'call' or 'put'")
+
+        return q
+
     def __generate_asset_grid(self):
         return np.linspace(0, self.__S_max, self.__s_nodes + 1)
 
     def __generate_time_grid(self):
         return np.linspace(0, self.__expiry, self.__t_nodes + 1)
 
+    def __set_boundary_conditions(self, q, T, tau):
+        if q == 1:
+            lower_boundary = 0
+            upper_boundary = self.__S_max - self.__K * np.exp(-self.__r * (self.__expiry - T[tau]))
+        else:
+            lower_boundary = self.__K * np.exp(-self.__r * (self.__expiry - T[tau]))
+            upper_boundary = 0
+
+        return lower_boundary, upper_boundary
+
     def get_V(self):
         return self.__V
 
 def main():
-    BlackScholesExplicitSolver(300, 1, 0.2, 0.05, 100, 20).solve().plot()
+    BlackScholesExplicitSolver('put', 300, 1, 0.2, 0.05, 100, 20).solve().plot()
 
 if __name__ == "__main__":
     main()
