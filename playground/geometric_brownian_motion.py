41559dc60257519b4cc63c65f22fccdd992e1bb3
chemardes <cdmrs08@gmail.com>
2024-12-08 20:56:15 +0000

    FEATURE: refactoring code

--- playground/geometric_brownian_motion.py
+++ playground/geometric_brownian_motion.py
@@ -1,33 +1,95 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# defining variables
-S0 = 100            # initial stock price
-mu = 0.05           # drift coefficient
-T = 1               # time in years
-sigma = 0.03        # volatility constant (how much the stock moves)
-sim = 10            # no of simulations
-dt = 1/365          # daily time steps
+class GeometricBrownianMotion:
 
-time_steps = int(T/dt)
-t = np.linspace(0, T, time_steps)
+    def __init__(self, S0, mu, sigma, T, time_steps, sim):
+        """
+        Initialize the Geometric Brownian Motion model with the given parameters.
 
-S = np.zeros((sim, time_steps))
+        Parameters:
+        - S0: Initial stock price
+        - mu: Drift coefficient (expected return)
+        - sigma: Volatility coefficient (standard deviation of returns)
+        - T: Time period for the simulation (in years)
+        - time_steps: Number of time steps in the simulation
+        - sim: Number of simulations to run
+        """
 
-# for all simulations at t = 0
-S[:,0] = S0
+        self.__S0 = S0
+        self.__mu = mu
+        self.__sigma = sigma
+        self.__T = T
+        self.__time_steps = time_steps
+        self.__sim = sim
+        self.__S = None
 
-for i in range(sim):
-    for j in range (1, time_steps):
-        # calculates based on the previous value at j-1
-        S[i,j] = S[i, j-1] * np.exp((mu - 0.5*sigma**2)*dt + sigma*np.sqrt(dt)*(np.random.normal(0, 1)))
+    def simulate_gbm(self):
+        """
+        Simulate the Geometric Brownian Motion for the given parameters.
 
-fig = plt.figure(figsize=(10,6))
-for i in range(sim):
-    plt.plot(t, S[i])
+        This method calculates the stock prices at each time step for each simulation.
+        """
 
-plt.title("Simulated Geometric Brownian Motion")
-plt.xlabel("Time (Years)")
-plt.ylabel("Stock Price")
-plt.show()
+        t = self.__generate_grid()
+        dt = t[1] - t[0]
+
+
+        S = np.zeros((self.__sim, self.__time_steps))
+
+        # for all simulations at t = 0
+        S[:,0] = self.__S0
+
+        for i in range(self.__sim):
+            for j in range (1, self.__time_steps):
+                # calculates stock price based on the previous value at j-1
+                S[i,j] = S[i, j-1] * np.exp((self.__mu - 0.5*self.__sigma**2)*dt + self.__sigma*np.sqrt(dt)*(np.random.normal(0, 1)))
+
+        self.__S = S
+
+        return self
+
+    def __generate_grid(self):
+        """
+        Generate a time grid from 0 to T with `time_steps` intervals.
+
+        Returns:
+        - A numpy array representing the time grid.
+        """
+
+        return np.linspace(0, self.__T, self.__time_steps)
+
+    def __get_stock_prices(self):
+        """
+        Get the simulated stock prices.
+
+        Returns:
+        - A numpy array of simulated stock prices.
+        """
+
+        return self.__S
+
+    def plot(self):
+        """
+        Plot the simulated stock prices for all simulations.
+        """
+
+        t = self.__generate_grid()
+        S = self.__get_stock_prices()
+
+        fig = plt.figure(figsize=(10,6))
+        for i in range(self.__sim):
+            plt.plot(t, S[i])
+
+        plt.title("Simulated Geometric Brownian Motion")
+        plt.xlabel("Time (Years)")
+        plt.ylabel("Stock Price")
+        plt.show()
+
+
+def main():
+    GeometricBrownianMotion(100, 0.05, 0.03, 1, 365, 10).simulate_gbm().plot()
+
+if __name__ == "__main__":
+    main()
 
