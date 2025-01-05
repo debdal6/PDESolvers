f14ddb7174ace50281eaa57321a7240b8ba3388e
chemardes <cdmrs08@gmail.com>
2025-01-05 15:29:08 +0000

    FEATURE: parallelising brownian motion (draft)

---
+++ GPUSolver/blackscholes/gbm_parallel.cu
@@ -0,0 +1,100 @@
+//
+// Created by Chelsea De Marseilla on 03/01/2025.
+//
+
+#include <cmath>
+#include <iostream>
+#include <curand_kernel.h>
+
+#define SEED 12345
+
+class GBMParallel
+{
+    private:
+        float initial_stock_price;
+        float mu;
+        float sigma;
+        float time;
+        int time_steps;
+        int num_of_simulations;
+
+    public:
+        GBMParallel(float initial_stock_price, float mu, float sigma, float time, int time_steps, int num_of_simulations)
+        {
+            this->initial_stock_price = initial_stock_price;
+            this->mu = mu;
+            this->sigma = sigma;
+            this->time = time;
+            this->time_steps = time_steps;
+            this->num_of_simulations = num_of_simulations;
+        }
+
+        // sets cuda kernel to run on gpu
+        __global__ static void simulate_gbm(float* grid, float* brownian_path, float initial_stock_price, float mu, float sigma, float time, int time_steps, int num_of_simulations)
+        {
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+            if (idx < num_of_simulations)
+            {
+
+                // initialising for random normal distribution
+                curandState state;
+                curand_init(SEED, idx, 0, &state);
+
+                float dt = time/static_cast<float>(time_steps);
+
+                brownian_path[idx * time_steps] = 0.0f;
+                grid[idx * time_steps] = initial_stock_price;
+
+                for (int i = 1; i < time_steps; i++)
+                {
+                    float Z = curand_normal(&state);
+                    brownian_path[idx * time_steps + i] = brownian_path[idx * time_steps + i - 1] + std::sqrt(dt) * Z;
+                    grid[idx * time_steps + i] = grid[idx * time_steps + i - 1] * expf((mu - 0.5f * powf(sigma, 2)) * dt + sigma * (brownian_path[idx * time_steps + i] - brownian_path[idx * time_steps + i - 1]));
+                }
+            }
+        }
+
+
+};
+
+int main()
+{
+
+    float initial_stock_price = 100.0f;
+    float mu = 0.05f;
+    float sigma = 0.03f;
+    float time = 1;
+    int time_steps = 365;
+    int num_of_simulations = 100;
+
+    int block_size = 256;
+    int num_blocks = (num_of_simulations + block_size - 1) / block_size;
+
+    size_t grid_size = num_of_simulations * time_steps;
+
+    // host memory allocation
+    float* host_grid = (float*)malloc(grid_size * sizeof(float));
+
+    // gpu memory allocation
+    float *dev_grid, *bm;
+    cudaMalloc(&dev_grid, grid_size * sizeof(float));
+    cudaMalloc(&bm, num_of_simulations * time_steps * sizeof(float));
+
+    // copy host memory to gpu memory
+    cudaMemcpy(dev_grid, host_grid, grid_size * sizeof(float), cudaMemcpyHostToDevice);
+
+    // kernel invocation
+    GBMParallel::simulate_gbm<<<num_blocks, block_size>>>(dev_grid, bm, initial_stock_price, mu, sigma, time, time_steps, num_of_simulations);
+
+    // waits kernel to finish all processes
+    cudaDeviceSynchronize();
+
+    // copy updated gpu memory to host memory
+    cudaMemcpy(host_grid, dev_grid,grid_size * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(dev_grid);
+    cudaFree(bm);
+
+    free(host_grid);
+}
