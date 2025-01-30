2858c70d8fcb50dba740702fd38311a14bc283b0
Debdal Chowdhury <53616883+debdal6@users.noreply.github.com>
2025-01-30 20:33:28 +0000

    Cleanup

--- playground/2D_explicit.py
+++ playground/2D_explicit.py
@@ -3,7 +3,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 
 # Global Constants
@@ -29,14 +28,14 @@ err_dt= np.abs((spaceStepSize**2)/(2*diffusivityConstant))
 print(err_dt)
 assert(timeStepSize <= err_dt)
 
-# Calculate lambda constants for x and y (they're the same if dx = dy)
+# Calculate lambda constant for x and y (they're the same if dx = dy)
 lambdaConstant = (diffusivityConstant * timeStepSize) / (spaceStepSize**2)
 # Stability condition: kdt/dx^2 <= 0.5
 assert(lambdaConstant <= 0.5) 
 print(lambdaConstant)
 
-# lambda functions for u0- initial condition and boundary condtions
-# 2 sin x sin 2y + 3 sin 4x sin 5y
+# lambda functions for initial condition and boundary condtions
+# 2 sin x sin 2y + 3 sin 4x sin 5y + t
 u0 = lambda x, y: 2 * np.sin(x) * np.sin(2*y) + 3 * np.sin(4*x) * np.sin(5*y)
 left = lambda t, y: 2 * np.sin(0) * np.sin(2*y) + 3 * np.sin(4*0) * np.sin(5*y) + t
 right = lambda t, y: 2 * np.sin(xLength) * np.sin(2*y) + 3 * np.sin(4*xLength) * np.sin(5*y) + t
@@ -46,27 +45,22 @@ top = lambda t, x: 2 * np.sin(x) * np.sin(2*yLength) + 3 * np.sin(4*x) * np.sin(
 
 # error assertion for intial & boundary conditions
 err1_u0 = np.abs(u0(0, 0) - left(0, 0))
-print(err1_u0)
 assert(err1_u0 < 1e-12)
 err2_u0 = np.abs(u0(0, yLength) - right(0, 0))
-print(err2_u0)
 assert(err2_u0 < 1e-12)
 err3_u0 = np.abs(u0(0, 0) - bottom(0, 0))
-print(err3_u0)
 assert(err3_u0 < 1e-12)
 err4_u0 = np.abs(u0(0, xLength) - top(0, 0))
-print(err4_u0)
 assert(err4_u0 < 1e-12)
 
 def initMatrix():
     # initialize an empty matrix
     matrix = np.empty((numPointsTime, numPointsSpace, numPointsSpace))
-    # set the x-axis boundary with lambda functions left and right with U not being symetrical
+    # set the boundary conditions for the entire 2D space
     for tau in range(numPointsTime):
         for i in range(numPointsSpace):
             matrix[tau, i, 0] = left(tau, xDomain[i])
             matrix[tau, i, -1] = right(tau, xDomain[i])
-    # set the y-axis boundary with lambda functions top and bottom with U not being symetrical
     for tau in range(numPointsTime):
         for j in range(numPointsSpace):
             matrix[tau, 0, j] = bottom(tau, yDomain[j])
@@ -82,7 +76,6 @@ def initMatrix():
 # Time-stepping loop
 def calculateTemperature(U):
     for tau in range(0, numPointsTime-1, 1):
-        # Loop over interior points (excluding boundaries)
         for i in range(1, numPointsSpace-1, 1):
             for j in range(1, numPointsSpace-1, 1):
                 # 5-point stencil implementation
@@ -117,11 +110,9 @@ ax = fig.add_subplot(111, projection='3d')
 def animate(k):
     plot_surface(tempMatrix[k], k, ax)
 
-# Initialize the matrix
 emptyMatrix = initMatrix()
-# Do the calculation here
 tempMatrix = calculateTemperature(emptyMatrix)
 
 
-anim = animation.FuncAnimation(fig, animate, interval=10, frames=numPointsTime, repeat=False)
+anim = FuncAnimation(fig, animate, interval=10, frames=numPointsTime, repeat=False)
 anim.save("heat_equation_solution_surf_explicit.gif", writer='pillow')
