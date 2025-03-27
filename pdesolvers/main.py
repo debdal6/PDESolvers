32cbbc8ab1d476241b36ca3557296e21b1483434
chemardes <cdmrs08@gmail.com>
2025-03-27 15:03:39 +0000

    FEATURE: handling gaps between trading dats

--- pdesolvers/main.py
+++ pdesolvers/main.py
@@ -1,10 +1,6 @@
 import numpy as np
 import pandas as pd
 import pdesolvers as pde
-from pdesolvers import HistoricalStockData
-from pdesolvers.optionspricing.market_data import OptionsData
-import matplotlib.pyplot as plt
-import yfinance as yf
 
 def main():
 
@@ -19,60 +15,37 @@ def main():
     # solver1 = pde.Heat1DCNSolver(equation1)
     # solver2 = pde.Heat1DExplicitSolver(equation1)
 
-    # testing for bse
-    equation2 = pde.BlackScholesEquation(pde.OptionType.EUROPEAN_CALL, 300, 1, 0.2, 0.05, 100, 100, 1000)
+    # testing for monte carlo pricing
 
-    file_path = '~/Downloads/out.csv'
+    ticker = 'AAPL'
 
-    gpu_solver = pde.GPUResults(file_path, 300, 1)
-    grid_data = gpu_solver.get_results()
-    # gpu_solver.plot_option_surface()
+    # STOCK
+    historical_data = pde.HistoricalStockData(ticker)
+    historical_data.fetch_stock_data( "2024-03-21","2025-03-21")
+    sigma, r = historical_data.estimate_metrics()
+    current_price = historical_data.get_latest_stock_price()
+
+    equation2 = pde.BlackScholesEquation(pde.OptionType.EUROPEAN_CALL, current_price, 1, sigma, r, 100, 100, 20000)
 
     solver1 = pde.BlackScholesCNSolver(equation2)
     solver2 = pde.BlackScholesExplicitSolver(equation2)
-    sol1 = solver1.solve().get_result()
-    # sol2 = solver2.solve().get_result()
-    # sol1.plot_greek(pde.Greeks.GAMMA)
-    diff = sol1 - grid_data
-    print(np.max(np.abs(diff)))
-
-
-
-# testing for monte carlo pricing
-
-    # ticker = 'AAPL'
-    # options = yf.Ticker(ticker)
-    # print(options)
-    # # STOCK
-    # historical_data = HistoricalStockData(ticker)
-    # historical_data.fetch_stock_data( "2024-03-21","2025-03-21")
-    # sigma, r = historical_data.estimate_metrics()
-    # current_price = historical_data.get_latest_stock_price()
-    #
-    # options = OptionsData(ticker)
-    # exp = options.get_earliest_expiration()
-    # strikes = options.get_strike_prices(exp)["2025-03-28"]
-    # plt.show()
-    #
-    # equation2 = pde.BlackScholesEquation(pde.OptionType.EUROPEAN_CALL, current_price, 1, sigma, r, 100, 100, 20000)
-
-    # solver1 = pde.BlackScholesCNSolver(equation2)
-    # solver2 = pde.BlackScholesExplicitSolver(equation2)
-    # sol1 = solver1.solve()
+    sol1 = solver1.solve()
     # sol1.plot()
 
     # COMPARISON
     #  look to see the corresponding option price for the expiration date and strike price
-    # pricing_1 = pde.BlackScholesFormula(pde.OptionType.EUROPEAN_CALL, current_price, 100, r, sigma, 1)
-    # pricing_2 = pde.MonteCarloPricing(pde.OptionType.EUROPEAN_CALL, current_price, 100, r, sigma, 1, 365, 10000)
-    #
-    # bs_price = pricing_1.get_black_scholes_merton_price()
-    # monte_carlo_price = pricing_2.get_monte_carlo_option_price()
-    # pde_price = sol1.get_result()[-1]
-    # print(f"PDE Price: {pde_price}")
-    # print(f"Black-Scholes Price: {bs_price}")
-    # print(f"Monte-Carlo Price: {monte_carlo_price}")
-    # pricing_2.plot()
+    pricing_1 = pde.BlackScholesFormula(pde.OptionType.EUROPEAN_CALL, current_price, 100, r, sigma, 1)
+    pricing_2 = pde.MonteCarloPricing(pde.OptionType.EUROPEAN_CALL, current_price, 100, r, sigma, 1, 365, 10000)
+
+    bs_price = pricing_1.get_black_scholes_merton_price()
+    monte_carlo_price = pricing_2.get_monte_carlo_option_price()
+
+    pde_price = sol1.get_result()[-1, 0]
+    print(f"PDE Price: {pde_price}")
+    print(f"Black-Scholes Price: {bs_price}")
+    print(f"Monte-Carlo Price: {monte_carlo_price}")
+    pricing_2.plot()
+
 
 if __name__ == "__main__":
     main()
--- pdesolvers/optionspricing/market_data.py
+++ pdesolvers/optionspricing/market_data.py
@@ -1,8 +1,6 @@
 import numpy as np
 import yfinance as yf
 
-from pdesolvers.enums.enums import OptionType
-
 class HistoricalStockData:
     def __init__(self, ticker):
         self.__stock_data = None
@@ -14,86 +12,25 @@ class HistoricalStockData:
         return self.__stock_data
 
     def estimate_metrics(self):
+
         if self.__stock_data is None:
-            print(f'No data available - data must be fetched first.')
-        #  dont drop the na values - u have to divide the log returns by the dt which is the interval (e.g if weekends itll be longer like 3 days)
+            raise ValueError(f'No data available - data must be fetched first.')
+
         self.__stock_data["Log Returns"] = np.log(self.__stock_data["Close"] / self.__stock_data["Close"].shift(1))
-        log_returns = self.__stock_data["Log Returns"].dropna()
 
-        sigma = log_returns.std()
-        mu = log_returns.mean()
+        # takes into account the gaps between trading days
+        self.__stock_data["Time Diff"] = self.__stock_data.index.to_series().diff().dt.days
+        self.__stock_data["Z"] = self.__stock_data["Log Returns"] / self.__stock_data["Time Diff"]
 
-        return sigma, mu
+        print(self.__stock_data)
+
+        annualized_sigma = self.__stock_data["Z"].std() * np.sqrt(252)
+        annualized_mu = self.__stock_data["Z"].mean() * 252
+
+        return annualized_sigma, annualized_mu
 
     def get_latest_stock_price(self):
         if self.__stock_data is None:
             raise ValueError("No data available. Call fetch_data first.")
 
         return self.__stock_data["Close"].iloc[-1].item()
-
-class OptionsData:
-
-    def __init__(self, ticker):
-        self.__ticker = ticker
-        self.__ticker_obj = yf.Ticker(ticker)
-
-    def fetch_call_options(self, exp_date):
-        return self.__ticker_obj.option_chain(date=exp_date).calls
-
-    def fetch_put_options(self, exp_date):
-        return self.__ticker_obj.option_chain(date=exp_date).puts
-
-    def fetch_all_options(self, exp_date):
-        return self.__ticker_obj.option_chain(date=exp_date)
-
-    def get_expirations(self):
-        return  self.__ticker_obj.options
-
-    def get_earliest_expiration(self):
-        return self.__ticker_obj.options[0]
-
-    def get_strike_prices(self, exp_date):
-        strike_prices = {}
-        calls = self.fetch_call_options(exp_date)
-        puts = self.fetch_put_options(exp_date)
-        strikes = {
-            'call' : calls['strike'].tolist(),
-            'put' : puts['strike'].tolist(),
-        }
-        strike_prices[exp_date] = strikes
-
-        return strike_prices
-
-    # maybe add a filter to for in-the-money, at-the-money, out-of-the-money
-    def get_nearest_strike_price(self, option_type: OptionType, stock_price, exp_date):
-
-        if option_type == OptionType.EUROPEAN_CALL:
-            options = self.fetch_call_options(exp_date)
-        elif option_type == OptionType.EUROPEAN_PUT:
-            options = self.fetch_put_options(exp_date)
-        else:
-            raise ValueError(f'Unsupported option type: {option_type}')
-
-        diff = np.abs(options['strike'] - stock_price)
-        nearest_idx = np.argmin(diff)
-        strike_price = options['strike'].iloc[nearest_idx]
-        return strike_price
-
-
-    def get_all_strike_prices(self):
-        strike_prices = {}
-
-        for exp_date in self.get_expirations():
-            calls = self.fetch_call_options(exp_date)
-            puts = self.fetch_put_options(exp_date)
-            strikes = {
-                'call' : calls['strike'].tolist(),
-                'put' : puts['strike'].tolist(),
-            }
-            strike_prices[exp_date] = strikes
-
-        return strike_prices
-
-
-
-
